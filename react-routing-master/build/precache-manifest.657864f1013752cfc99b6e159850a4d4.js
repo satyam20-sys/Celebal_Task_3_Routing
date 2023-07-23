89236d4484fddaba86c32b6095a4ce41cfb02975
Satyam Sinha <sinhasatyam662@gmail.com>
2023-07-24 00:01:16 +0530

    added files

---
+++ react-routing-master/build/precache-manifest.657864f1013752cfc99b6e159850a4d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75274beb6fcfbc557d41773c13c11474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7df290aaedce80645ece",
+    "url": "/static/css/main.ad5ad3d1.chunk.css"
+  },
+  {
+    "revision": "9d5234893b4a01fbc0af",
+    "url": "/static/js/2.c39091b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c39091b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df290aaedce80645ece",
+    "url": "/static/js/main.a8ea3250.chunk.js"
+  },
+  {
+    "revision": "f74123f82deeca44253a",
+    "url": "/static/js/runtime-main.21301aca.js"
+  }
+]);
